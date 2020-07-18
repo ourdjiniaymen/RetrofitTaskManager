305402b49bc6cd432f0dbd17b7d0470d3b82a65c
ourdjini aymen <ga_ourdjini@esi.dz>
2020-07-18 14:57:09 +0100

    initialization

---
+++ app/src/main/java/com/example/retrofittaskmanager/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.retrofittaskmanager.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retrofittaskmanager.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
